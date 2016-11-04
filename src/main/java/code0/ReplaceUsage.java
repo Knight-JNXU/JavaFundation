c714434db0789b30f2718e20d28bf3afd9612db7
Knight-JXNU <1990785022@qq.com>
2016-11-04 11:30:30 +0800

    微调

--- src/main/java/code0/ReplaceUsage.java
+++ src/main/java/code0/ReplaceUsage.java
@@ -2,11 +2,13 @@ package code0;
 
 /**
  * Created by Knight_JXNU on 2016/10/12.
+ * string replace
  */
 public class ReplaceUsage {
     @org.junit.Test
     public void test(){
         String x = "fmn";
+        //replace 后的结果需要用 string 接收
         String y = x.replace('f', 'F');
         y = y+"wxy";
         System.out.println(y);
