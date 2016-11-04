42de3ea099f6fcb0541c8836f11c51b651bab083
Knight-JXNU <1990785022@qq.com>
2016-11-04 14:09:26 +0800

    Arrays.toString 打印数组

---
+++ src/main/java/code0/ArraysPrint.java
@@ -0,0 +1,17 @@
+package code0;
+
+import java.util.Arrays;
+
+/**
+ * Created by Knight_JXNU on 2016/11/2.
+ * java Arrays.toString 打印数组
+ */
+public class ArraysPrint {
+
+    @org.junit.Test
+    public void test(){
+        int[] arrays = {1, 2, 3, 4, 5, 6};
+        System.out.println(Arrays.toString(arrays));
+    }
+
+}
