a84e8e5dd6b7e97713c9581a9160163336b90b78
Knight-JXNU <1990785022@qq.com>
2016-11-08 10:40:44 +0800

    冒泡排序优化2

---
+++ src/main/java/Algorithms/BubbleSortOp2.java
@@ -0,0 +1,45 @@
+package Algorithms;
+
+import until.OutputUtil;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by Knight_JXNU on 2016/11/8.
+ * 优化冒泡排序2
+ * 时间复杂度：最好是O(1)，最坏是O(n^2)
+ * 空间复杂度：O(1)
+ * input demo:
+ * 1 2 3 4 5 6
+ */
+public class BubbleSortOp2 {
+    public static void main(String[] args){
+        OutputUtil.print("冒泡排序：");
+        OutputUtil.print("输入一个数组，用空格分隔：");
+        Scanner sc = new Scanner(System.in);
+        String inputStr = sc.nextLine();
+        String strs[] = inputStr.split(" ");
+        int array[] = new int[strs.length];
+        for(int i=0; i<strs.length; i++){
+            array[i] = Integer.parseInt(strs[i]);
+        }
+        int index = strs.length-1;
+        while (index > 0){
+            for (int i=0; i<strs.length-1; i++){
+                int k = index;
+                index = 0;
+                for(int j=0; j<k; j++){
+                    if(array[j] > array[j+1]){
+                        int temp = array[j];
+                        array[j] = array[j+1];
+                        array[j+1] = temp;
+                        index = j;  //记录无序区的末尾位置
+                    }
+                }
+            }
+        }
+        OutputUtil.print("从小到大排序后");
+        OutputUtil.print(Arrays.toString(array));
+    }
+}
