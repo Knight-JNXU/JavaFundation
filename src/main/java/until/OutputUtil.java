968af07c6ff81ce614df2bb6dc51d7c533658e55
Knight-JXNU <1990785022@qq.com>
2016-11-08 10:10:03 +0800

    工具类

---
+++ src/main/java/until/OutputUtil.java
@@ -0,0 +1,15 @@
+package until;
+
+/**
+ * Created by Knight_JXNU on 2016/11/8.
+ */
+public class OutputUtil {
+    /**
+     * 统一输出方法
+     * -.-怎么觉得有点矫情
+     * @param str
+     */
+    public static void print(String str){
+        System.out.println(str);
+    }
+}
