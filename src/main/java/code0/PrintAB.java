9c0d9bb1eee9c2180e0f4b69851848350a06c8cf
Knight_JNXU <1990785022@qq.com>
2016-11-09 22:10:03 +0800

    在知乎上看到的一题比较有趣的百度面试题

---
+++ src/main/java/code0/PrintAB.java
@@ -0,0 +1,44 @@
+package code0;
+
+/**
+ * Created by Knigh on 2016/11/9.
+ * 先执行if，再执行else
+ * 问题来源：https://www.zhihu.com/question/50801791
+ * 往if中填入一个条件，使下列的if-else语句输出ab
+     if(){
+         System.out.print("a");
+     }else{
+     System.out.print("b");
+     }
+ */
+public class PrintAB {
+
+    public static void main(String[] args){
+        if((new Object(){
+            public boolean foo() {
+                System.out.print("a");
+                return false;
+            }
+        }).foo()){
+            System.out.print("a");
+        }else{
+            System.out.print("b");
+        }
+        System.out.println("\n-----------");
+        if(System.out.printf("a")==null){
+            System.out.print("a");
+        }else{
+            System.out.print("b");
+        }
+        System.out.println("\n-----------");
+        if(f()){
+            System.out.print("a");
+        }else{
+            System.out.print("b");
+        }
+    }
+    private static boolean f(){
+        System.out.print("a");
+        return false;
+    }
+}
