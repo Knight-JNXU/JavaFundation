8d22af85b179d41671a7e305969a86d96faf9452
Knight-JXNU <1990785022@qq.com>
2016-11-10 11:24:19 +0800

    添加矩阵输入的公共方法

--- src/main/java/until/CommonUtil.java
+++ src/main/java/until/CommonUtil.java
@@ -1,5 +1,6 @@
 package until;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -41,6 +42,21 @@ public class CommonUtil {
     }
 
     /**
+     * 把一行字符串转换成征信数组
+     * @param str
+     * @return
+     */
+    public static int[] line2IntArr(String str){
+        String strs[] = str.split(" ");
+        int array[] = new int[strs.length];
+        int len = strs.length;
+        for(int i=0; i<len; i++){
+            array[i] = Integer.parseInt(strs[i]);
+        }
+        return array;
+    }
+
+    /**
      * 输入一个int
      * @return
      */
@@ -48,5 +64,27 @@ public class CommonUtil {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         return n;
+    }
+
+    /**
+     * 统一输入矩阵
+     * @return
+     */
+    public static int[][] inputIntMatrix(){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<int[]> list = new ArrayList<int[]>();
+        String inputStr;
+        while (sc.hasNextLine() && !(inputStr=sc.nextLine()).equals("end")){
+            list.add(line2IntArr(inputStr));
+        }
+        int rowN = list.size();
+        int columnN = list.get(0).length;
+        int matrix[][] = new int[rowN][columnN];
+        for(int i=0; i<rowN; i++){
+            for(int j=0; j<columnN; j++){
+                matrix[i][j] = list.get(i)[j];
+            }
+        }
+        return matrix;
     }
 }
