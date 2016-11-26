2a94732fa1a6fbe42089346d231523ad58f750e8
Knight_JNXU <1990785022@qq.com>
2016-11-26 20:27:50 +0800

    2016-11-26 挖财笔试题

---
+++ src/main/java/practice/WaCaiTest00.java
@@ -0,0 +1,12 @@
+package practice;
+
+/**
+ * Created by Knigh on 2016/11/26.
+ * 挖财的一题笔试
+ */
+public class WaCaiTest00 {
+    public void test(){
+        //float i = 0.0;  //这一行就开始报错
+        //i +=1.0;
+    }
+}
