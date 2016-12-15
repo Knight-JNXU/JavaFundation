95585967c377b9413bdff01c2668c1da0ae8b0d9
Knight-JXNU <1990785022@qq.com>
2016-12-15 14:52:17 +0800

    赛马网顺丰2016真题

---
+++ src/main/java/practice/Z.java
@@ -0,0 +1,25 @@
+package practice;
+
+/**
+ * Created by Knight_JXNU on 2016/12/15.
+ */
+class X{
+    Y y = new Y();
+    public X(){
+        System.out.print("X");
+    }
+}
+class Y{
+    public Y(){
+        System.out.print("Y");
+    }
+}
+public class Z {
+    Y y = new Y();
+    public Z(){
+        System.out.print("Z");
+    }
+    public static void main(String[] args){
+        new Z();
+    }
+}
