38afae844651b8055aaa28e0349ab2ea3004f15b
Knight-JXNU <1990785022@qq.com>
2016-12-16 10:51:30 +0800

    对象声明赋值

---
+++ src/main/java/practice/NewCodeTest05.java
@@ -0,0 +1,11 @@
+package practice;
+
+/**
+ * Created by Knight_JXNU on 2016/12/16.
+ */
+public class NewCodeTest05 {
+//    final i1;  //编译错
+    static int i2;
+    static final int i3=234;
+    final float i=3.14f;
+}
