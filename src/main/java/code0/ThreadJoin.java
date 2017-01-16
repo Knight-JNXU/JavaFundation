1bbda01929a9ba0b272b3160314cc801187d34d3
Knight-JXNU <1990785022@qq.com>
2017-01-16 10:01:47 +0800

    探究 stringbuffer capacity 的作用

---
+++ src/main/java/code0/ThreadJoin.java
@@ -0,0 +1,36 @@
+package code0;
+
+import org.junit.Test;
+
+/**
+ * Created by Knight_JXNU on 2017/1/16.
+ * 探究Thread join的作用
+ * result：
+ * my thread
+ * test thread
+ * 分析：join会把子线程加入到主线程之前执行，只有在子线程执行完了，主线程才能继续执行
+ */
+public class ThreadJoin {
+    private class MyThread extends Thread{
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(5000);
+                System.out.println("my thread");
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+    @Test
+    public void test(){
+        try {
+            MyThread myThread = new MyThread();
+            myThread.start();
+            myThread.join();
+            System.out.println("test thread");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
