1ab5ff3bd55e2e9cecd28563a9695bb5bd90944e
Knight-JXNU <1990785022@qq.com>
2017-01-17 09:34:52 +0800

    short 强制类型转换为 byte

---
+++ src/main/java/practice/NewCodeTest06.java
@@ -0,0 +1,16 @@
+package practice;
+
+import org.junit.Test;
+
+/**
+ * Created by Knight_JXNU on 2017/1/17.
+ */
+public class NewCodeTest06 {
+    @Test
+    public void test(){
+        short a = 128;
+        byte b = (byte)a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
