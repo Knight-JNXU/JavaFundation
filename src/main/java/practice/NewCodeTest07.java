4ca7ab4f51a95852184cd566bbe90497e18646a3
Knight-JXNU <1990785022@qq.com>
2017-02-07 10:51:03 +0800

    ceil floor 符号问题

---
+++ src/main/java/practice/NewCodeTest07.java
@@ -0,0 +1,15 @@
+package practice;
+
+import org.junit.Test;
+
+/**
+ * Created by Knight_JXNU on 2017/2/7.
+ */
+public class NewCodeTest07 {
+    @Test
+    public void test(){
+        double d1 = -0.5;
+        System.out.println("Ceil d1="+Math.ceil(d1));
+        System.out.println("floor d1="+Math.floor(d1));
+    }
+}
