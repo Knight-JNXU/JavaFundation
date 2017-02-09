5911a4329fe441b0a55aa94421bf7b9cdeb03ab1
Knight-JXNU <1990785022@qq.com>
2017-02-09 10:33:28 +0800

    ceil、floor结果的符号和原数据的符号是一致的

--- src/main/java/code0/CeilFloorSymbol.java
+++ src/main/java/code0/CeilFloorSymbol.java
@@ -4,8 +4,13 @@ import org.junit.Test;
 
 /**
  * Created by Knight_JXNU on 2017/2/7.
+ * 探究ceil、floor的符号问题
+ * Console：
+ * Ceil d1=-0.0
+ * floor d1=-1.0
+ * 分析：ceil floor结果的符号和原数据的符号是一致的
  */
-public class NewCodeTest07 {
+public class CeilFloorSymbol {
     @Test
     public void test(){
         double d1 = -0.5;
