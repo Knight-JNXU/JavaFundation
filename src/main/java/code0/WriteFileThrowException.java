58dc05a7e7df268ae65288670a5940d559e55f42
Knight-JXNU <1990785022@qq.com>
2017-02-16 14:18:53 +0800

    探究在写文件时抛出异常被写的文件会出现什么情况

--- src/main/java/code0/WriteFileThrowException.java
+++ src/main/java/code0/WriteFileThrowException.java
@@ -8,18 +8,20 @@ import java.io.FileOutputStream;
 /**
  * Created by Knight_JXNU on 2016/11/25.
  * 探究在写文件时抛出异常被写的文件会出现什么情况
+ * result：
+ * 写入失败，期间试图写入的东西没有被写入文件
  */
 public class WriteFileThrowException {
     private void writeFile() throws Exception{
         FileOutputStream out = null;
         try {
-            String filePath = "E:/test.txt";
+            String filePath = "E:/AtomicType.txt";
             File file = new File(filePath);
             if(!file.exists()){
                 file.createNewFile();
             }
             out  = new FileOutputStream(file, true);
-            out.write("test".getBytes("utf-8"));
+            out.write("AtomicType".getBytes("utf-8"));
             throw new Exception();
         }catch (Exception e){
             e.printStackTrace();
