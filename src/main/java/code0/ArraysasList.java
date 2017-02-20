42bfda789ad62c4966ae1db9091eca857e18807d
Knight-JXNU <1990785022@qq.com>
2017-02-20 10:36:32 +0800

    探究asList

--- src/main/java/code0/ArraysasList.java
+++ src/main/java/code0/ArraysasList.java
@@ -1,16 +1,28 @@
 package code0;
 
+import org.junit.Test;
+
 import java.util.Arrays;
 import java.util.List;
 
 /**
  * Created by Knight_JXNU on 2016/12/5.
  * 探究asList
+ * console:
+ * 1
+ * [I@477afcff
+ * 分析：
+ * int[] 转为 list 之后，类型变成了 int[]
  */
 public class ArraysasList {
-    private void test(){
+    @Test
+    public void test(){
         int[] array = {1,2,3,4};
         //这个的返回值类型是 Arrays.ArrayList class，这个 class 实现了 List 接口
         List list = Arrays.asList(array);
+        System.out.println(list.size());
+        for(Object obj : list){
+            System.out.println(obj);
+        }
     }
 }
