3266e1dfb2086689396d75503c03736b927a3c84
Jumbo <Jumbo@10.88.75.192>
2017-03-29 17:58:31 +0800

    new conf

--- src/main/java/JpaTest.java
+++ src/main/java/JpaTest.java
@@ -1,6 +1,15 @@
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.junit.Before;
 import org.junit.Test;
 
 public class JpaTest {
@@ -8,6 +17,19 @@ public class JpaTest {
 	@Test
 	public void returnReturnResPath(){
 //		System.out.println(Class.getResource());
+	}
+	
+	private SessionFactory sessionFactory;
+	private Session session;
+	private Transaction transaction;
+	
+	@Before
+	public void init(){
+	    StandardServiceRegistry standardServiceRegistry = new StandardServiceRegistryBuilder().configure().build();
+	    Metadata metadata = new MetadataSources(standardServiceRegistry).getMetadataBuilder().applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE).build();
+	    sessionFactory = metadata.getSessionFactoryBuilder().build();
+	    session = sessionFactory.openSession();
+	    transaction = session.beginTransaction();
 	}
 	
 	@Test
